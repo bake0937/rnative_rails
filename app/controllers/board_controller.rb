fef9fa971857c97629abd5b21381965c31b1638f
bake0937 <sadayasu.info@gmail.com>
2018-01-21 15:49:22 +0900

    add readBoardAndUser

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,2 +1,14 @@
 class BoardController < ApplicationController
+  def readBoardAndUser
+    response = {status:true}
+
+    boards = Board.all.order("created_at DESC")
+    users = User.all.order("id")
+    response[:boards] = boards
+    response[:users] = users
+
+    render :json=> response
+  end
+
+
 end
