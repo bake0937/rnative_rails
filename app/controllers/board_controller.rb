db3db1e09e956f713f9ed78f8457bcb8d2810096
bake0937 <sadayasu.info@gmail.com>
2018-01-21 15:58:27 +0900

    add readComments

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -18,4 +18,14 @@ class BoardController < ApplicationController
 
     render :json=> response
   end
+
+  def readComments
+    response = {status:true}
+    response["X-CSRF-Token"] = form_authenticity_token
+
+    recs = Comment.eager_load(:user).where("board_id = ?", params[:board_id]).order("comments.created_at DESC").as_json(include: :user)
+    response["rows"] = recs
+
+    render :json=> response
+  end
 end
